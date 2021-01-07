f822ce9dbac2b008a9a3b9292543f4e62165b6aa
CSjianbel <jiankarlo99@gmail.com>
2021-01-07 21:33:09 +0800

    Added: License, buil-project, requirements.txt, .gitignore - Modified: README, ccpt.py

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
-# Create C++ Competitive Programming Template 
+# Create C++ Competitive Programming Template
+
+Generates a C++ Competitive Programming Template File.<br />
 
 ## Credits for Template
 
+Template from https://gist.github.com/kodekracker/e09f9d23573f117a5db0a
 
+- Author: Akshay Pratap Singh
+- Handle: code_crack_01
+
+## Clone
+
+```bash
+git clone https://github.com/CSjianbel/create-cp-template.git
+```
+
+## Setup
+
+```bash
+chmod +x build-project
+
+./build-project
+```
+
+## Usage
+
+```bash
+./ccpt [outfile_path.cpp]
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+### License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+_A Project by Jiankarlo A. Belarmino_
--- ccpt.py
+++ ccpt.py
@@ -8,17 +8,19 @@ if len(sys.argv) != 2:
 TEMPLATE_PATH = "./template.cpp"
 OUTFILE_PATH = sys.argv[1];
 
-if re.search(".\w+", OUTFILE_PATH):
-    OUTFILE_PATH = re.sub(".\w+", ".cpp", OUTFILE_PATH)
+if re.search("\.\w+$", OUTFILE_PATH):
+    OUTFILE_PATH = re.sub("\.\w+$", ".cpp", OUTFILE_PATH)
 else:
-    print("Invalid Filetype")
-    sys.exit(2)
+    OUTFILE_PATH += ".cpp"
 
-with open(OUTFILE_PATH, "w") as outfile:
-
-    with open(TEMPLATE_PATH, "r") as template:
-        data = template.read()
-        outfile.write(data)
+try:
+    with open(OUTFILE_PATH, "w") as outfile:
+        with open(TEMPLATE_PATH, "r") as template:
+            data = template.read()
+            outfile.write(data)
+except OSError:
+    print("Invalid Filepath")
+    sys.exit(3)
     
 print(f"Created CP template on {OUTFILE_PATH}")
 
