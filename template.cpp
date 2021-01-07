2b09bfa5320418848639dac9d074b152554fb6d1
CSjianbel <jiankarlo99@gmail.com>
2021-01-08 03:11:44 +0800

    Created Own Template

--- template.cpp
+++ template.cpp
@@ -1,13 +1,3 @@
-// Template from https://gist.github.com/kodekracker/e09f9d23573f117a5db0a
-/*
-* Note: This template uses some c++11 functions , so you have to compile it with c++11 flag.
-*       Example:-   $ g++ -std=c++11 c++Template.cpp
-*
-* Author : Akshay Pratap Singh
-* Handle: code_crack_01
-*
-*/
- 
 /********   All Required Header Files ********/
 #include <iostream>
 #include <string>
@@ -21,7 +11,9 @@
 #include <list>
 #include <stack>
 #include <map>
+#include <unordered_map>
 #include <set>
+#include <unordered_set>
 #include <functional>
 #include <numeric>
 #include <utility>
@@ -35,48 +27,18 @@
 
 using namespace std;
 
-/*******  All Required define Pre-Processors and typedef Constants *******/
-#define SCD(t) scanf("%d",&t)
-#define SCLD(t) scanf("%ld",&t)
-#define SCLLD(t) scanf("%lld",&t)
-#define SCC(t) scanf("%c",&t)
-#define SCS(t) scanf("%s",t)
-#define SCF(t) scanf("%f",&t)
-#define SCLF(t) scanf("%lf",&t)
-#define MEM(a, b) memset(a, (b), sizeof(a))
+#define ll long long
 #define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
 #define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
 #define REP(i, j) FOR(i, 0, j, 1)
 #define RREP(i, j) RFOR(i, j, 0, 1)
-#define all(cont) cont.begin(), cont.end()
-#define rall(cont) cont.end(), cont.begin()
 #define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
-#define IN(A, B, C) assert( B <= A && A <= C)
-#define MP make_pair
-#define PB push_back
 #define INF (int)1e9
 #define EPS 1e-9
 #define PI 3.1415926535897932384626433832795
 #define MOD 1000000007
 #define read(type) readInt<type>()
 const double pi=acos(-1.0);
-typedef pair<int, int> PII;
-typedef vector<int> VI;
-typedef vector<string> VS;
-typedef vector<PII> VII;
-typedef vector<VI> VVI;
-typedef map<int,int> MPII;
-typedef set<int> SETI;
-typedef multiset<int> MSETI;
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int  uint64;
-
-/****** Template of some basic operations *****/
-template<typename T, typename U> inline void amin(T &x, U y) { if(y < x) x = y; }
-template<typename T, typename U> inline void amax(T &x, U y) { if(x < y) x = y; }
-/**********************************************/
 
 /****** Template of Fast I/O Methods *********/
 template <typename T> inline void write(T x)
@@ -116,62 +78,21 @@ template <typename T> inline T readInt()
 }
 /************************************/
 
-
-/******* Debugging Class Template *******/
-#define DEBUG
-
-#ifdef DEBUG
-
-    #define debug(args...)     (Debugger()) , args
-
-    class Debugger
-    {
-        public:
-        Debugger(const std::string& _separator = " - ") :
-        first(true), separator(_separator){}
-
-        template<typename ObjectType> Debugger& operator , (const ObjectType& v)
-        {
-            if(!first)
-                std:cerr << separator;
-            std::cerr << v;
-            first = false;
-            return *this;
-        }
-        ~Debugger() {  std:cerr << endl;}
-
-        private:
-        bool first;
-        std::string separator;
-    };
-
-#else
-    #define debug(args...)                  // Just strip off all debug tokens
-#endif
-
-/**************************************/
-
 /******** User-defined Function *******/
+void solve()
+{
 
-
-/**************************************/
-
+}
 
 /********** Main()  function **********/
 int main()
 {
-
-	#ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);
-	//freopen("output.txt","w",stdout);
-	#endif
-
-	int tc;
-	tc = read(int);
-
-	while(tc--){
-		write(tc);
+	int t, c = 1;
+	while (t--)
+	{
+		cout << "Case #" << c++ << ": ";
+		solve();
 	}
 	return 0;
 }
-/********  Main() Ends Here *************/
+
