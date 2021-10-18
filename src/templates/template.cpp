75d47d6be1f49b62064fdafc1b05e85034d33bac
CSjianbel <jiankarlo99@gmail.com>
2021-10-18 10:28:58 +0800

    Added type redefintions & member/method macros

--- src/templates/template.cpp
+++ src/templates/template.cpp
@@ -1,29 +1,44 @@
-/********   All Required  Files ********/
+// Include libraries
 #include <bits/stdc++.h>
 
-/********   Macros & Constants  ********/
-#define ll long long
+// Macros
 #define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
 #define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
 #define REP(i, j) FOR(i, 0, j, 1)
 #define RREP(i, j) RFOR(i, j, 0, 1)
 #define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define read(type) readInt<type>()
+
+// Constants
 #define INF (int)1e9
 #define EPS 1e-9
 #define PI 3.1415926535897932384626433832795
 #define MOD 1000000007
-#define read(type) readInt<type>()
 const double pi=acos(-1.0);
+
+// member/method macros
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
 using namespace std;
 
-/********** Solution **********/
+// Shorten typenames
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef pair<int,int> pii;
+typedef map<int,int> mii;
+
+// Solution
 void solve()
 {
 	// Code Here
 }
 
-/********** Main()  function **********/
+// Main function
 int main()
 {
 	ios::sync_with_stdio(0);
